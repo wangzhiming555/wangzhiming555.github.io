a038cdb28adfd7bbbb0c37e4a54d798fbb9e302c
wangzhiming555 <949618567@qq.com>
2017-05-18 20:26:34 +0800

    first

--- huituban/js/draw.js
+++ huituban/js/draw.js
@@ -49,13 +49,33 @@ class Draw{
     }
     circlein(ox,oy,mx,my){
         this.init();
+        this.cobj.beginPath();
+        //求半径  内切圆  1.鼠标移动的x距离大于y移动的距离  这是圆的半径是y的一半 y可能为负 去绝对值
+        //                   2.鼠标移动的y距离大于x移动的距离  这是圆的半径是x的一半 x可能为负 去绝对值
+        var r=Math.abs(mx-ox)>Math.abs(my-oy)?Math.abs(my-oy)/2:Math.abs(mx-ox)/2;
+        //半径可能为负  需要判断
+        this.cobj.arc(ox+(mx>ox?r:-r),oy+(my>oy?r:-r),r,0,Math.PI*2);
+        this.cobj[this.style]();
+    }
+    duo(ox,oy,mx,my,side) {
+        this.init();
         this.cobj.save();
         this.cobj.translate(ox,oy);
+        this.cobj.rotate(Math.PI/2);//旋转画布  使得多边形的上边是平的
+        var angle=Math.PI/side;//旋转的角度
+        var r=Math.sqrt(Math.pow(mx-ox,2)+Math.pow(my-oy,2));
+        var x=r*Math.cos(angle);//根据半径 角度（三角形中）算出x.y  坐标
+        var y=r*Math.sin(angle);
+//        开始绘制
         this.cobj.beginPath();
-        var r=(mx-ox)/2>(my-oy)/2?(my-oy)/2:(mx-ox)/2;
-        this.cobj.arc(mx>ox?r:-r,my>oy?r:-r,r,0,2*Math.PI);
+        this.cobj.moveTo(x,y);//起始点
+//        下一个点 然后旋转画布 继续画线
+        for(let i=0;i<side;i++){
+            this.cobj.lineTo(x,-y);
+            this.cobj.rotate(-angle*2);
+        }
         this.cobj[this.style]();
-        this.cobj.restore;
+        cobj.restore();
     }
     // duo(s,r)
     // }
